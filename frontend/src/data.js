758c391f36efb961c2d109045df01bf14cc94945
cjbs05428 <ch.jbabu28@gmail.com>
2021-01-19 12:52:08 +0530

    Share Code on Github

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data={
+    products: [
+        {
+            _id:'1',
+            name:'Star-wars Soldier 1 Set',
+            category:'Battery Items',
+            image:'./images/p1.jpg',
+            price:300,
+            brand:'Starwars',
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Product'
+        },
+        {
+            _id:'2',
+            name:'Apple Fiber',
+            category:'Non Battery Items',
+            image:'./images/p2.jpg',
+            price:100,
+            brand:'Green Apple',
+            rating:4.3,
+            numReviews:14,
+            description:'Good Quality Product'
+        },
+        {
+            _id:'3',
+            name:'Tin Driving Car',
+            category:'Battery Items',
+            image:'./images/p3.jpg',
+            price:500,
+            brand:'Tin Tin',
+            rating:4.8,
+            numReviews:30,
+            description:'High Quality Product'
+        },
+        {
+            _id:'4',
+            name:'Ninja Turtles Action',
+            category:'Non Battery Items',
+            image:'./images/p4.jpg',
+            price:300,
+            brand:'Ninja Toys',
+            rating:4.5,
+            numReviews:50,
+            description:'High Quality Product'
+        },
+        {
+            _id:'5',
+            name:'Drums For Kids',
+            category:'Non Battery Items',
+            image:'./images/p5.jpg',
+            price:300,
+            brand:'Music Cuts',
+            rating:4.5,
+            numReviews:25,
+            description:'High Quality Product'
+        },
+        {
+            _id:'6',
+            name:'Star-wars Soldier All Set',
+            category:'Battery Items',
+            image:'./images/p6.jpg',
+            price:1000,
+            brand:'Starwars',
+            rating:4.9,
+            numReviews:100,
+            description:'High Quality Product'
+        },
+    ],
+};
+export default data;
